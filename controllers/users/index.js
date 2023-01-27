4f908340fd105fa7baf87135f6d15503c37ef040
Maryna-Dr <drapata.mb@gmail.com>
2023-01-27 16:32:38 +0200

    Created an endpoint to retrieve transaction categories

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,10 +2,12 @@ const getCurrentUser = require('./getCurrentUser');
 const updateSubscription = require('./updateSubscription');
 const updateAvatar = require('./updateAvatar');
 const resendVerifyUserEmail = require('./resendVerifyUserEmail');
+const getCategoriesList = require('./getCategoriesList');
 
 module.exports = {
   getCurrentUser,
   updateSubscription,
   updateAvatar,
   resendVerifyUserEmail,
+  getCategoriesList,
 };
