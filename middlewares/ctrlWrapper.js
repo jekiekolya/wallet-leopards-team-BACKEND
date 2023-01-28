88f8a9de67eddb7e203a196c27df59cbfc09abdf
Mykola Berezhniuk <bermukola@gmail.com>
2023-01-28 21:55:30 +0200

    Update ctrlWrapper.js

--- middlewares/ctrlWrapper.js
+++ middlewares/ctrlWrapper.js
@@ -1,10 +1,8 @@
 const ctrlWrapper = ctrl => {
   return async (req, res, next) => {
-    console.log(ctrl);
     try {
       await ctrl(req, res, next);
     } catch (error) {
-      console.log('erron in wrapper');
       next(error);
     }
   };
