775606e2db32cbe1f01b8ae18b5db753dbc1df74
Leon7422 <y.kurduban98@gmail.com>
2023-02-01 21:50:55 +0200

    addRandomColorCategory

--- src/categoriesList.js
+++ src/categoriesList.js
@@ -2,42 +2,52 @@ const categoriesList = [
   {
     _id: '1',
     name: 'Main expenses',
+    color: '#0f0f70',
   },
   {
     _id: '2',
     name: 'Products',
+    color: '#0aa8c1',
   },
   {
     _id: '3',
     name: 'Car',
+    color: '#0e6658',
   },
   {
     _id: '4',
     name: 'Self care',
+    color: '#8a9ff2',
   },
   {
     _id: '5',
     name: 'Child care',
+    color: '#79fd41',
   },
   {
     _id: '6',
     name: 'Household products',
+    color: '#690a70',
   },
   {
     _id: '7',
     name: 'Education',
+    color: '#dde90e',
   },
   {
     _id: '8',
     name: 'Leisure',
+    color: '#9dcab1',
   },
   {
     _id: '9',
     name: 'Entertainment',
+    color: '#87bb55',
   },
   {
     _id: '10',
     name: 'Other expenses',
+    color: '#ca456e',
   },
 ];
 
